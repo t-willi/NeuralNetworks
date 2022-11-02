3ede1349d7f06f921c57a5b6e94d7c1d35b264e1
t-willi <twilli.gnl@gmail.com>
2022-11-02 18:58:59 +0100

    Update unzip.py

--- unzip.py
+++ unzip.py
@@ -1,11 +1,9 @@
-def unzip(save_path=None,zip_path=None):
+def unzip(save_path=None,zip_path=None,reload=True):
   from pathlib import Path
   import zipfile
-  # Setup path to data folder
-  if isinstance(save_path, str):
-    train_path = Path(save_path)
-  if save_path == None:
-    train_path = Path("data/train_data")
+
+  train_path = Path(save_path)
+  
   #train_path = data_path / "train_data"
   # If the image folder doesn't exist, download it and prepare it... 
   if train_path.is_dir():
@@ -14,8 +12,9 @@ def unzip(save_path=None,zip_path=None):
       print(f"Did not find {train_path} directory, creating one...")
       train_path.mkdir(parents=True, exist_ok=True)
   # Unzip 
-  zip_path=Path(zip_path)
-  with zipfile.ZipFile(zip_path, "r") as zip_ref:
-      print(f"Unzipping data to folder...") 
-      zip_ref.extractall(train_path)
-      print("unzip is finished")
+  if reload is True:
+    zip_path=Path(zip_path)
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        print(f"Unzipping data to folder...") 
+        zip_ref.extractall(train_path)
+        print("unzip is finished")
