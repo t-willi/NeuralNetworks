76dda3722166ff82c6e318c201e02e0f4e00e9f4
t-willi <twilli.gnl@gmail.com>
2022-11-03 13:04:22 +0100

    Update dataloader_PTB_XL.py
    my own dataloader, but able to open PTB-XL data

--- dataloader_PTB_XL.py
+++ dataloader_PTB_XL.py
@@ -1,63 +1,75 @@
-import os
-from torch.utils.data import Dataset, DataLoader
+import glob
 import pandas as pd
 import torch
-import torch.nn as nn
-import numpy as np
-import glob
-import wfdb # to read ecgs of Physionet
+import wfdb 
 from wfdb import processing # to normalize
-class ECGDataSimple(Dataset):
-    def __init__(self, data_dir, norm=[-1,1], cropping=None, transform=None):
-        
-        #self.all_ecg_files = []
-        self.norm = norm
-        self.norm_lb = norm[0]
-        self.norm_ub = norm[1]
-        self.cropping = cropping
-        
-        #for data_dir in data_dirs:
-        self.ecg_files = glob.glob(data_dir + "/*/*.dat")
-        # self.all_ecg_files = self.all_ecg_files + ecg_files
-
-        self.transform = transform
+class Custom_dataset_PTB():
+    def __init__(self, data_dir,max_value=5011,column=3,split=True,target="train",size=1):
+      #get all files from directory loaded in all_files list
+      self.column=column
+      self.max_value=max_value
+      self.size=size
+      #should shuffle the data here?
+      self.files = glob.glob(data_dir + "/*/*.dat")
+      self.len=int((len(self.files))*self.size)
+      #print(f"len:{self.len}")
+      self.cut1=int(self.len*0.8)
+      #print(f"cut1:{self.cut1}")
+      self.cut2=int(self.len*0.9)
+      #print(f"cut2:{self.cut2}")
+      self.train_files=self.files[0:self.cut1]
+      self.test_files=self.files[self.cut1:self.cut2]
+      self.val_files=self.files[self.cut2:self.len]
+      self.target=target
+      self.split=split
 
     def __len__(self):
-        return len(self.ecg_files)
+      if self.split is True:
+        if self.target is "train":
+          return len(self.train_files)
+        if self.target is "test":
+          return len(self.test_files)
+        if self.target is "val":
+          return len(self.val_files)
+      if self.split is not True:
+        return len(self.files)
 
-    def __getitem__(self, idx):
-        
-        # Get file name of asc file
-        file_name = self.ecg_files[idx][:-4] #remove .dat at the end 
-        
-        ecg_signals = wfdb.rdsamp(file_name)[0]
+    def __getitem__(self,idx):
+      #header = ["1","2","v1","v2","v3","v4","v5","v6"]
+      header=["I", "II", "III", "aVF", "aVR", "aVL", "V1", "V2", "V3", "V4", "V5", "V6"]
+      #turn list of dataframes into Tensor
+      if self.split is True:
+        if self.target is "train":
+          file_name = self.train_files[idx][:-4]
+          temp_df=pd.DataFrame(wfdb.rdsamp(file_name)[0], columns = header)
+        if self.target is "test":
+          file_name = self.test_files[idx][:-4]
+          temp_df=pd.DataFrame(wfdb.rdsamp(file_name)[0], columns = header)
+        if self.target is "val":
+          file_name = self.val_files[idx][:-4]
+          temp_df=pd.DataFrame(wfdb.rdsamp(file_name)[0], columns = header)
 
-        if self.norm:
-            processing.normalize_bound(ecg_signals, lb=self.norm_lb, ub=self.norm_ub)
-
-        ecg_signals = torch.tensor(ecg_signals) # convert to tensor
-        
       
-        ecg_signals = ecg_signals.float()
-        
-        #print(ecg_signals.shape)
-        #ecg_signals = ecg_signals  / self.norm_num # Normalizing aplitude of voltage levels 
+      temp_list_in=temp_df.iloc[:,0]
+      #temp_list_in=normalize([temp_list_in], norm="max")
+      temp_tensor_in = torch.tensor(temp_list_in,dtype=torch.float32)
+      temp_tensor_in=temp_tensor_in.unsqueeze(0)
+      #load label Tensor
+      temp_list_out=temp_df.iloc[:,[1,6,7,8,9,10,11]].values
+      #temp_list_out=normalize([temp_list_out], norm="max")
+      temp_tensor_out=torch.tensor(temp_list_out,dtype=torch.float32)
+      temp_tensor_out=temp_tensor_out.unsqueeze(0)
+      temp_tensor_out=torch.permute(temp_tensor_out,(0,2,1))
+      #combine input and label and output
+      temp_tensor_pair= temp_tensor_in,temp_tensor_out
+      return temp_tensor_pair
 
-        #cropping
-        if self.cropping:
-            ecg_signals = ecg_signals[self.cropping[0]:self.cropping[1], :]
-        
-        # Transposing the ecg signals
-        ecg_signals = ecg_signals.t() 
-        #ecg_signals = ecg_signals.unsqueeze(0)
-     
-  
 
-        if self.transform:
-            ecg_signals = self.transform(ecg_signals)
-            #sample = self.transform(sample["ecg_signals"])
-
-        # Return sample at the end
-        sample = {'ecg_signals': ecg_signals}
-
-        return sample
+def make_loader(dataset,batch_size):
+  from torch.utils.data import DataLoader
+  loader = DataLoader(dataset,
+                      batch_size=batch_size,
+                      shuffle=True,
+                      drop_last=True
+                      )
+  return loader
